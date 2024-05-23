6acf2fcf64d7a762be61e91ee653cef9a669ab5c
tjsiv <tjsivelsjr@gmail.com>
2024-05-22 23:22:50 -0700

    Added cors to server file

--- server.js
+++ server.js
@@ -2,11 +2,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const cors = require('cors')
 
 // CONFIGURATION
 require('dotenv').config();
 const PORT = process.env.PORT;
 app.use(express.json())
+
+//cors call
+app.use(cors())
 
 //mongoose call
 mongoose.connect(process.env.MONGO_URI)
